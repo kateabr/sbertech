1d208531edcbbd1c55ef0785b8eee8d47819e903
kateabr <kateabr@yandex.ru>
2021-09-20 20:01:36 +0300

    hw10

---
+++ src/main/java/sbertech/hw10/MultithreadedFactorial.java
@@ -0,0 +1,19 @@
+package sbertech.hw10;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class MultithreadedFactorial {
+    private static BigDecimal factorial(int x) {
+        if (x < 2)
+            return BigDecimal.ONE;
+        return BigDecimal.valueOf(x).multiply(factorial(x - 1));
+    }
+
+    public static void main(String[] args) throws IOException {
+        Files.lines(Paths.get("factorials.txt"))
+                .forEach(x -> System.out.printf("%s: %s\n", x, factorial(Integer.parseInt(x)).toString()));
+    }
+}
